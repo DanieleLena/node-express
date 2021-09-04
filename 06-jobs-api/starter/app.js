b7bf03f2ad6a9ecd9f4bb36464840a2eb38c3bcc
DanieleLena <daniele.lena@yahoo.it>
2021-09-04 16:30:00 +0200

    Final commit

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
+
+//security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express')
 const app = express()
 const connectDB = require('./db/connect');
@@ -16,6 +23,11 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({windowMs:60 * 1000,max:60}));
+
 // extra packages
 
 app.use('/api/v1/auth',authRouter);
